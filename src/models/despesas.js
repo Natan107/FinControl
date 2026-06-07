ac6692f8a01806fa5c3f0160395706f2b33abc80
Matheus <robertomatheus082@gmail.com>
2026-06-07 00:09:28 -0300

    chore: Adicionando dados mock de despesas

---
+++ src/models/despesas.js
@@ -0,0 +1,30 @@
+export const despesas = [
+  {
+    id: 1,
+    descricao: "Mercado",
+    valor: 450,
+    categoria: "Alimentação",
+    data: "2026-06-03"
+  },
+  {
+    id: 2,
+    descricao: "Conta de Luz",
+    valor: 120,
+    categoria: "Contas",
+    data: "2026-06-01"
+  },
+  {
+    id: 3,
+    descricao: "Uber",
+    valor: 930,
+    categoria: "Transporte",
+    data: "2026-06-02"
+  },
+  {
+    id: 4,
+    descricao: "Cinema",
+    valor: 30,
+    categoria: "Lazer",
+    data: "2026-06-05"
+  }
+];
